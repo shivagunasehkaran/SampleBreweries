dae29dc7a7dcc828630465ec8fba3fd56bdc5d78
Shiva Gunasehkaran <danielshiva@gmail.com>
2020-01-19 21:13:28 +0530

    Updated styles in Themes folder

---
+++ src/themes/colors/index.js
@@ -0,0 +1,13 @@
+const colors = {
+    clear: 'rgba(227,227,227,1)',
+    cardBackground: '#FFFFFF',
+    primaryText: '#000000',
+    secondaryText: '#686868',
+    divider: '#CCCCCC',
+    header: 'rgba(250,250,250,0.87)',
+    black: 'black',
+    white: 'white',
+    buttonColor: 'orange',
+};
+
+export default colors;
